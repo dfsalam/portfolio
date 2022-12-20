402ce8a99c42bc55a9d80f1c32539caf99cd3b4a
dfsalam <dfsalam@gmail.com>
2022-12-20 14:35:36 -0500

    Add hamburguer menu with style and functionality

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const nav = document.querySelector('.nav-menu');
 const menu = document.querySelector('.hamb-menu');
+const close = document.querySelector('.hamb--close');
+const hTagsa= document.querySelector('.hamb--ha');
+const hTagsb= document.querySelector('.hamb--hb');
+const hTagsc= document.querySelector('.hamb--hc');
 
 function hambClick() {
   menu.removeAttribute('hidden');
 }
 
+function closeClick() {
+  menu.setAttribute('hidden', 'hidden');
+}
+
 nav.addEventListener('click', hambClick);
+close.addEventListener('click', closeClick);
+hTagsa.addEventListener('click', closeClick);
+hTagsb.addEventListener('click', closeClick);
+hTagsc.addEventListener('click', closeClick);
