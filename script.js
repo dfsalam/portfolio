04f0d11b78ded9be533e716d713ca5ce01042dea
dfsalam <dfsalam@gmail.com>
2022-12-20 16:13:38 -0500

    Fix linter errors

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const menu = document.querySelector('.hamb-menu');
 const close = document.querySelector('.hamb--close');
 const hTagsa = document.querySelectorAll('.hamb--h');
 
-
 function hambClick() {
   menu.removeAttribute('hidden');
 }
@@ -15,8 +14,6 @@ function closeClick() {
 nav.addEventListener('click', hambClick);
 close.addEventListener('click', closeClick);
 
-hTagsa.forEach(element =>  {
+hTagsa.forEach((element) => {
   element.addEventListener('click', closeClick);
-})
-
-
+});
