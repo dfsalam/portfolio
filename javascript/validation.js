dbaac071a2596b9dad87b93b231dcf73392bb5df
dfsalam <dfsalam@gmail.com>
2022-12-23 17:36:30 -0500

    Fix linter errors

--- javascript/validation.js
+++ javascript/validation.js
@@ -1,18 +1,15 @@
-const email = document.querySelector('.form-email')
-const button = document.querySelector('.btn')
-const label = document.querySelector('.form--error')
+const email = document.querySelector('.form-email');
+
+const label = document.querySelector('.form--error');
 const frmContact = document.getElementById('frmContact');
 
 frmContact.addEventListener('submit', (e) => {
-    e.preventDefault();
-    label.style.display = 'none';
-    if (email.value === email.value.toLowerCase()){
-        frmContact.submit();
-        return;
-    }
+  e.preventDefault();
+  label.style.display = 'none';
+  if (email.value === email.value.toLowerCase()) {
+    frmContact.submit();
+    return;
+  }
 
-    label.style.display = 'block';
+  label.style.display = 'block';
 });
-
-
-
